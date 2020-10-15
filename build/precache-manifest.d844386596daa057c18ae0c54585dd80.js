d9f8c17f29dc422b238eaa1ce1d1ad509c9a802e
Joao Simoes <jdl.simoes@campus.fct.unl.pt>
2020-10-15 21:43:22 +0200

    uibuild

---
+++ build/precache-manifest.d844386596daa057c18ae0c54585dd80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92797de4c2a2db0da0170d1653d9c760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54dd32709c192453a110",
+    "url": "/static/js/2.d98781c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98781c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d843ce39437ab24e39",
+    "url": "/static/js/main.d1273c52.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
